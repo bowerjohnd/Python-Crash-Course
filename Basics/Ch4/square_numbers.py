1294735fe42765e527e953dce4dc807f9f032eb2
bowerjohnd <bowerjohnd@gmail.com>
2026-09-24 15:47:05 -0400

    lists: range, stats(min/max/sum), list comprehension

---
+++ Basics/Ch4/square_numbers.py
@@ -0,0 +1,25 @@
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+
+print(squares)
+
+# meant for interpreter
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+# back to squares.py
+
+# list comprehension
+squares = [value**2 for value in range(1,11)]
+print(squares)
