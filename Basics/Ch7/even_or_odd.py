5787fcb4aa38d97f35d017a004fac6e57a204dd0
bowerjohnd <bowerjohnd@gmail.com>
2026-09-25 22:14:01 -0400

    modulus and check if user-input is even or odd

---
+++ Basics/Ch7/even_or_odd.py
@@ -0,0 +1,17 @@
+# interpreter work
+
+print(4 % 3)
+print(5 % 3)
+print(6 % 3)
+print(7 % 3)
+
+# even_or_odd.py
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
+
