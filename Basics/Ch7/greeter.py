9da5df76c071ce9cbba21dcff6118055621a653d
bowerjohnd <bowerjohnd@gmail.com>
2026-09-25 22:16:30 -0400

    user-input, prompt string, cast int()

---
+++ Basics/Ch7/greeter.py
@@ -0,0 +1,15 @@
+name = input("Please enter your name: ")
+print(f"\nHello, {name}")
+
+prompt = "If you share your name, we can personalize the mesages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+# interpreter work
+
+age = input("How old are you? ")
+print(age)
+age = int(age)
+print(age >= 18)
